377a4bec390aa4269fd26e137bf461daf321bf73
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-11-08 15:50:46 +0000

    update

--- app/main.py
+++ app/main.py
@@ -1,29 +1,37 @@
 from services.usuario_service import UsuarioService
 from repositories.usuario_repositories import UsuarioRepository
 from config.database import Session
-from sqlalchemy.orm import Session
 from models.usuario_models import Usuario
-
+import os
 
 def main():
         session = Session()
         repository = UsuarioRepository(session)
         service = UsuarioService(repository)
+
+        def listar_usuario():
+            print("Listando todos usuários cadastrados")
+            lista_usuarios = service.listar_todos_usuarios()
+            for usuario in lista_usuarios:
+                print(f"Nome: {usuario.nome} - E-mail: {usuario.email} - Senha: {usuario.senha}")
+                
+
         while True:
-
-
+            # os.system("cls || clear")
             print("\n=== SENAI SOLUTION ===")
-            print("Código  |   Função")
-            print("1 - Adicionar usuário")
-            print("2 - Pesquisar um usuário")
-            print("3 - Atualizar dados de um usuário")
-            print("4 - Excluir um usuário")
-            print("5 - Exibir todos os usuários cadastrados")
-            print("0 - Sair")
+            print("\nCódigo\t|\tFunção")
+            print("  1\t|  Adicionar usuário")
+            print("  2\t|  Pesquisar um usuário")
+            print("  3\t|  Atualizar dados de um usuário")
+            print("  4\t|  Excluir um usuário")
+            print("  5\t|  Exibir todos os usuários cadastrados")
+            print("  0\t|  Sair")
+
             opcao = int(input("\nEscolha uma opção: "))
 
-            match (opcao):
+            match(opcao):
                 case 1:
+                    os.system("cls || clear")
                     print("\nAdicionar usuário")
                     nome = input("Digite o nome do usuário: ")
                     email = input("Digite o email do usuário: ")
@@ -32,55 +40,36 @@ def main():
                     service.criar_usuario(nome=nome, email=email, senha=senha)
 
                 case 2:
-                    print("\nPesquisar dados de apenas um usuário")
-                    email = input("Digite o email do usuário que será consultado: ")
-
-                    usuario = session.query(Usuario).filter_by(email = email).first()
-
-                    if usuario:
-                        print(f"{usuario.nome} - {usuario.email} - {usuario.senha}")
-                    else:
-                        print(f"Usuário não existe")
+                    os.system("cls || clear")
+                    listar_usuario()
+                    print("\nPesquisando usuário...")
+                    service.pesquisar_usuario_unico()
 
                 case 3:
-                    print("\nAtualizar dados de um usuário")
-                    email = input("Digite o email do usuário que deseja atualizar: ")
-
-                    usuario = session.query(Usuario).filter_by(email=email).first()
-
-                    if usuario:
-                        usuario.nome = input("Digite seu nome: ")
-                        usuario.email = input("Digite seu e-mail: ")
-                        usuario.senha = input("Digite sua senha: ")
-
-                    else:
-                        print("Usuário não encontrado. ")
+                    os.system("cls || clear")
+                    listar_usuario()
+                    service.atualizar_usuario()
 
                 case 4:
-                    print("\nExcluindo os dados de um usuário.")
-                    email = input("Digite o e-mail do usuário que será excluido: ")
-
-                    usuario = session.query(Usuario).filter_by(email=email).first()
-
-                    if usuario:
-                        session.delete(usuario)
-                        session.commit()
-                        print(f"Usuário {usuario.nome} excluido com sucesso!")
-                    else:
-                        print("Usuário não encontrado.")
+                    os.system("cls || clear")
+                    listar_usuario()
+                    print("\nExcluindo usuário...")
+                    service.excluir_usuario()
 
                 case 5:
-                    print("Exibir todos od usuários cadatrados")
-                    lista_usuarios = service.listar_todos_usuarios()
-                    for usuario in lista_usuarios:
-                        print(f"{usuario.nome} - {usuario.email} - {usuario.senha}")
+                    os.system("cls || clear")
+                    listar_usuario()
 
                 case 0:
-                    print("Sair")
+                    os.system("cls || clear")
+                    print(f"Sair...")
+                    break
 
                 case _:
+                    os.system("cls || clear")
                     print(f"Opção inválida - Tente novamente")
 
 
-            if __name__ == "__main__":
-                main()
+if __name__ == "__main__":
+    os.system("cls || clear")
+    main()
--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -1,5 +1,6 @@
 from models.usuario_models import Usuario
 from repositories.usuario_repositories import UsuarioRepository
+import os
 
 
 class UsuarioService:
@@ -19,6 +20,7 @@ class UsuarioService:
 
             self.repository.salvar_usuario(usuario)
             print("Usuário cadastrado com sucesso!")
+
         except TypeError as erro:
             print(f"Erro ao salvar o usuário: {erro}")
         except Exception as erro:
@@ -27,12 +29,33 @@ class UsuarioService:
     def listar_todos_usuarios(self):
         return self.repository.listar_usuarios()
     
+    def excluir_usuario(self):
+        try:
+            email = input("Digite o e-mail do usuário: ")
+            usuario_cadastrado = self.repository.pesquisar_usuario_por_email(email)
+
+            if usuario_cadastrado:
+                senha = input("Informe a senha do usuário para exclui-lo: ")
+
+                if senha == usuario_cadastrado.senha:
+                    self.repository.excluir_usuario(usuario_cadastrado)
+                    print("Usuário excluido com suceso!")
+                print("Usuário não encontrado!")
+                return
+            else:
+                print("Usuário não cadastrado!")
+                return
+        
+        except TypeError as erro:
+            print(f"Erro ao excluir usuário: {erro}")
+        except Exception as erro:
+            print(f"Erro inesperado: {erro}")
+    
     def atualizar_usuario(self):
         try:
             print("\nAtualizando os dados de um usuário.")
 
             email_usuario = input("Informe o email do usuário: ")
-
             usuario_cadastrado = self.repository.pesquisar_usuario_por_email(email=email_usuario)
 
             if usuario_cadastrado:
@@ -47,3 +70,22 @@ class UsuarioService:
             print(f"Erro ao atualizar o usuário: {erro}")
         except Exception as erro:
             print(f"Ocorreu um erro inesperado: {erro}")
+
+    def pesquisar_usuario_unico(self):
+        try:
+            email = input("\nDigite o email do usuário: ")
+            usuario_cadastrado = self.repository.pesquisar_usuario_por_email(email)
+
+            if usuario_cadastrado:
+                os.system("cls || clear")
+                print(" - Dados do usuário - ")
+                print(f"Usuário: {usuario_cadastrado.nome} - {usuario_cadastrado.email} - {usuario_cadastrado.senha}")
+                return
+            else:
+                print("Usuário não encontrado!")
+                return
+            
+        except TypeError as erro:
+            print(f"Erro ao procurar o usuário: {erro}")
+        except Exception as erro:
+            print(f"Erro inesperado: {erro}")
