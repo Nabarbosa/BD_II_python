f027d48a8f9a463645109b0be97a305c7658e027
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2024-11-09 03:43:17 +0000

    so sei que nada sei (￣o￣) . z Z

--- app/models/usuario_models.py
+++ app/models/usuario_models.py
@@ -20,10 +20,6 @@ class Usuario(Base):
         self.email = email
         self.senha = senha
 
-    def _verifcar_nome_invalido(self, valor):
-        if not isinstance(valor, str):
-            raise TypeError("O nome deve ser um texto")
-        
     def _verificar_nome_vazio(self, valor):
         try:
             self._verifcar_nome_invalido(valor)
@@ -34,6 +30,45 @@ class Usuario(Base):
             print(f"Erro inesperado: {erro}")
         self.nome = valor
         return self.nome
+    
+    def _verificar_nome_vazio(self, nome):
+        if not nome.strip():
+            raise ValueError("O que está sendo solicitado, está vazio!")
+    
+    def _verifcar_nome_invalido(self, valor):
+        if not isinstance(valor, str):
+            raise TypeError("O que está sendo solicitado, deve ser um texto.")
+        
+
+    def _verificar_email_usuario(self, email):
+        self._verificar_email_invalido(email)
+        self._verificar_email_vazio(email)
+
+        self.email = email
+        return self.email 
+    
+    def _verificar_senha_usuario(self, senha):
+        self._verificar_senha_invalida(senha)
+        self._verificar_senha_vazio(senha)
+
+        self.senha = senha
+        return self.senha
+    
+    def _verificar_email_invalido(self, email):
+        if not isinstance(email, str):
+            raise TypeError("O que está sendo solicitado está inválido!")
+        
+    def _verificar_email_vazio(self, email):
+        if not email.strip():
+            raise TypeError("O que está sendo solicitado está vazio!")
+
+    def _verificar_senha_invalida(self, senha):
+        if not isinstance(senha, str):
+            raise TypeError("O que está sendo solicitado está inválido!")
+        
+    def _verificar_senha_vazio(self, senha):
+        if not senha.strip():
+            raise TypeError("O que está sendo solicitado  está vazio")
 
 # Criando tabela no banco de dados.
 Base.metadata.create_all(bind=db)
--- app/test/test_usuario.py
+++ app/test/test_usuario.py
@@ -15,9 +15,26 @@ def test_usuario_senha(usuario_valido):
     assert usuario_valido.senha == "123"
 
 def test_nome_usuario_invalido(usuario_valido):
-    with pytest.raises(TypeError, match="O que estasendo solicitado, deve ser um texto."):
+    with pytest.raises(TypeError, match="O que está sendo solicitado deve ser um texto."):
         Usuario(10, "marta@gmail.com", "123")
 
 def test_nome_usuario_vazio(usuario_valido):
     with pytest.raises(TypeError, match="O que está sendo solicitado está vazio!"):
-        Usuario(" ", "marta@gmail.com", "123")
+        Usuario(" ", "marta@gmail.com", "123") 
+
+def test_email_usuario_invalido(usuario_valido):
+    with pytest.raises(TypeError, match="O que está sendo solicitado deve ser um texto."):
+        Usuario("Marta", 10, "123")
+
+def test_email_usuario_vazio(usuario_valido):
+    with pytest.raises(TypeError, match="O que está sendo solicitado está vazio!"):
+        Usuario("Marta", "", "123") 
+
+def test_senha_usuario_invalido(usuario_valido):
+    with pytest.raises(TypeError, match="O que está sendo solicitado deve ser um texto."):
+        Usuario("Marta", "marta@gmail.com", 123)
+
+def test_senha_usuario_vazio(usuario_valido):
+    with pytest.raises(TypeError, match="O que está sendo solicitado está vazio!"):
+        Usuario("Marta", "marta@gmail.com", "") 
+
